821ce44efc7c7211d1a1b4bd181e6979b982375e
Rihazul <rihazulislam@gmail.com>
2023-11-27 20:18:46 -0500

    Loading screen under sign-in added

--- Prototype/app/src/main/java/com/example/prototype/SignInActivity.java
+++ Prototype/app/src/main/java/com/example/prototype/SignInActivity.java
@@ -3,14 +3,17 @@ package com.example.prototype;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.material.progressindicator.CircularProgressIndicator;
 import com.google.android.material.textfield.TextInputLayout;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
@@ -42,18 +45,27 @@ public class SignInActivity extends AppCompatActivity {
         rememberMeButton = findViewById(R.id.rememberMeButton);
         forgotPasswordButton = findViewById(R.id.forgotPasswordButton);
         passwordInputLayout = findViewById(R.id.passwordInputLayout);
+        ProgressBar progressIndicator = findViewById(R.id.progress_sign_in);
 
+        progressIndicator.setVisibility(View.GONE);
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 String email = emailInput.getText().toString();
                 String password = passwordInput.getText().toString();
 
-                if (validateLoginInformation(email, password)) {
-                    signInUser(email, password);
-                } else {
-                    Toast.makeText(SignInActivity.this, R.string.please_enter_valid_email_and_password, Toast.LENGTH_LONG).show();
-                }
+                progressIndicator.setVisibility(View.VISIBLE);
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        if (validateLoginInformation(email, password)) {
+                            signInUser(email, password);
+                        } else {
+                            Toast.makeText(SignInActivity.this, R.string.please_enter_valid_email_and_password, Toast.LENGTH_LONG).show();
+                            progressIndicator.setVisibility(View.GONE);
+                        }
+                    }
+                },4000);
             }
         });
 
