2a56d7ecde396792233787e4b0fcbeb08fecd9ec
WasWasHere <144953406+WasWasHere@users.noreply.github.com>
2023-11-28 20:40:57 -0500

    to review

--- Prototype/app/build.gradle.kts
+++ Prototype/app/build.gradle.kts
@@ -57,4 +57,9 @@ dependencies {
     annotationProcessor ("com.github.bumptech.glide:compiler:4.12.0")
 
     implementation ("com.journeyapps:zxing-android-embedded:4.3.0")
+
+    //Add the dependency for the Realtime messaging
+    implementation 'com.google.firebase:firebase-database:20.3.0'
+    implementation 'com.google.firebase:firebase-inappmessaging:20.4.0'
+
 }
